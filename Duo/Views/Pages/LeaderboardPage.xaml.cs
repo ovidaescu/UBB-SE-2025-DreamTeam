668c693a5cf5a722331552e1e8a78abe09dcce88
DavidCroitor <146849106+DavidCroitor@users.noreply.github.com>
2025-03-27 11:17:50 +0200

    Update LeaderboardPage.xaml.cs

--- Duo/Views/Pages/LeaderboardPage.xaml.cs
+++ Duo/Views/Pages/LeaderboardPage.xaml.cs
@@ -33,7 +33,7 @@ public sealed partial class LeaderboardPage : Page
     public ObservableCollection<LeaderboardEntry> Leaderboard { get; set; }
     private LeaderBoardViewModel _leaderBoardViewModel;
     private string _selectedMode = "Global";
-    private int currentUserId = 1;
+    private int currentUserId = App.CurrentUser.Id;
     public LeaderboardPage()
     {
         this.InitializeComponent();
